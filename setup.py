7b692f05a1ff9f75d1b42a57af2a4e5bf67e5f0c
manmartgarc <manmartgarc@gmail.com>
2019-07-10 13:24:20 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 13:20:03 2019
+
+===============================================================================
+@author:    Manuel Martinez
+===============================================================================
+"""
+from setuptools import setup
+
+setup(name='stochatreat',
+      version='0.1.0',
+      description='Randomized block assignment using pandas',
+      url='https://github.com/manmartgarc/stochatreat',
+      author='Manuel Martinez',
+      author_email='manmartgarc@gmail.com',
+      license='MIT',
+      packages=['stochatreat'],
+      zip_safe=False)
