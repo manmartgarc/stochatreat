4d171662c878d5cf37d8a5c7f7af00b9978fb50e
manmartgarc <manmartgarc@gmail.com>
2019-07-10 13:35:20 -0400

    change version to 0.0.1

--- stochatreat/__init__.py
+++ stochatreat/__init__.py
@@ -8,7 +8,7 @@ Created on Wed Jul 10 12:16:55 2019
 ===============================================================================
 """
 
-__version__ = '0.1.0'
+__version__ = '0.0.1'
 __author__ = 'Manuel Martinez'
 
 from .stochatreat import stochatreat
