8cbd80cee04a258533f4b1fe676a33fd5c7db807
Tobias Schmidt <royalts@gmail.com>
2019-07-11 15:08:24 +0200

    black

--- tests/test_stochatreat.py
+++ tests/test_stochatreat.py
@@ -22,41 +22,38 @@ def df(request):
 
     return df
 
-@pytest.mark.parametrize('n_treats', [2, 3, 4, 5, 10])
-@pytest.mark.parametrize("block_cols", [['dummy'], ['dummy', 'block1'], ['block1', 'block2']])
+
+@pytest.mark.parametrize("n_treats", [2, 3, 4, 5, 10])
+@pytest.mark.parametrize(
+    "block_cols", [["dummy"], ["block1"], ["block1", "block2"]]
+)
 def test_stochatreat_no_probs(n_treats, block_cols, df):
     """Test that overall treatment assignment proportions across all strata are as intended with equal treatment assignment probabilities"""
     treats = stochatreat(
-        data=df,
-        block_cols=block_cols,
-        treats=n_treats,
-        idx_col='id',
-        random_state=42)
-
-    treatment_shares = treats.groupby(['treat'])['id'].count() / treats.shape[0]
-
-    np.testing.assert_almost_equal(
-        treatment_shares,
-        np.array([1/n_treats] * n_treats),
-        decimal=3
+        data=df, block_cols=block_cols, treats=n_treats, idx_col="id", random_state=42
     )
 
-@pytest.mark.parametrize('probs', [[0.1, 0.9], [0.5, 0.5], [0.9, 0.1]])
-@pytest.mark.parametrize("block_cols", [['dummy'], ['dummy', 'block1'], ['block1', 'block2']])
+    treatment_shares = treats.groupby(["treat"])["id"].count() / treats.shape[0]
+
+    np.testing.assert_almost_equal(
+        treatment_shares, np.array([1 / n_treats] * n_treats), decimal=3
+    )
+
+
+@pytest.mark.parametrize("probs", [[0.1, 0.9], [0.5, 0.5], [0.9, 0.1]])
+@pytest.mark.parametrize(
+    "block_cols", [["dummy"], ["block1"], ["block1", "block2"]]
+)
 def test_stochatreat_probs(probs, block_cols, df):
     """Test that overall treatment assignment proportions across all strata are as intended with unequal treatment assignment probabilities"""
     treats = stochatreat(
         data=df,
         block_cols=block_cols,
         treats=len(probs),
-        idx_col='id',
+        idx_col="id",
         probs=probs,
-        random_state=42)
-
-    treatment_shares = treats.groupby(['treat'])['id'].count() / treats.shape[0]
-
-    np.testing.assert_almost_equal(
-        treatment_shares,
-        np.array(probs),
-        decimal=3
+        random_state=42,
     )
+    treatment_shares = treats.groupby(["treat"])["id"].count() / treats.shape[0]
+
+    np.testing.assert_almost_equal(treatment_shares, np.array(probs), decimal=3)
