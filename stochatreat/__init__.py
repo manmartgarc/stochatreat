fd45bf75924c4650fcbb00701281b67d0cb97382
manmartgarc <manmartgarc@gmail.com>
2019-07-23 09:49:32 -0400

    bump version

--- stochatreat/__init__.py
+++ stochatreat/__init__.py
@@ -8,7 +8,7 @@ Created on Wed Jul 10 12:16:55 2019
 ===============================================================================
 """
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 __author__ = 'Manuel Martinez'
 
 from .stochatreat import stochatreat
