8a5691eecb8d7e63e8e40c2929c92ea09a965f45
Manuel Martinez <manmartgarc@gmail.com>
2021-07-08 20:43:42 -0400

    fix linting length

--- stochatreat/utils.py
+++ stochatreat/utils.py
@@ -1,17 +1,22 @@
-from math import gcd
 from fractions import Fraction
 from functools import reduce
+from math import gcd
+from typing import Iterable
 
-def lcm(ints):
-    """Helper function to compute the Lowest Common Multiple of a list of integers"""
+
+def lcm(ints: Iterable[int]):
+    """
+    Helper function to compute the Lowest Common Multiple of a list of integers
+    """
     return reduce(lambda a, b: a * b // gcd(a, b), ints)
 
 
-def get_lcm_prob_denominators(probs):
-    """Helper function to compute the LCM of the denominators of the probabilities"""
+def get_lcm_prob_denominators(probs: Iterable[float]):
+    """
+    Helper function to compute the LCM of the denominators of the probabilities
+    """
     prob_denominators = [
         Fraction(prob).limit_denominator().denominator for prob in probs
     ]
     lcm_prob_denominators = lcm(prob_denominators)
     return lcm_prob_denominators
-
