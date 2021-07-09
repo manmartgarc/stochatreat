9d2424cfe832a5920c1a1e688e5b2c4d71edfa7e
Manuel Martinez <manmartgarc@gmail.com>
2021-07-08 20:15:42 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,40 +6,60 @@ Created on Wed Jul 10 13:20:03 2019
 @author:    Manuel Martinez
 ===============================================================================
 """
-import stochatreat
+import codecs
 import os
-import setuptools
 import sys
+from pathlib import Path
+
+from setuptools import find_packages, setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel')
     sys.exit()
 
+
+def read(rel_path: str) -> str:
+    here = Path(__file__).parent.absolute()
+    with codecs.open(str(here.joinpath(rel_path)), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path=rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
+
+
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-setuptools.setup(
-        name='stochatreat',
-        version=stochatreat.__version__,
-        author='Manuel Martinez',
-        author_email='manmartgarc@gmail.com',
-        description='Randomized block assignment using pandas',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url='https://github.com/manmartgarc/stochatreat',
-        keywords=[
-            'randomization',
-            'block randomization'
-             ],
-        install_requires=[
-            'pandas',
-            'numpy',
-            'pytest',
-            ],
-        packages=setuptools.find_packages(),
-        classifiers=[
-                'Programming Language :: Python :: 3',
-                'License :: OSI Approved :: MIT License',
-                'Operating System :: OS Independent',
-                ]
-        )
+setup(
+    name='stochatreat',
+    version=get_version('stochatreat/__init__.py'),
+    author='Manuel Martinez',
+    author_email='manmartgarc@gmail.com',
+    description='Stratified random assignment using pandas',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/manmartgarc/stochatreat',
+    keywords=[
+        'randomization',
+        'block randomization',
+        'stratified randomization'
+        'stratified',
+        'strata',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
