95eb2a4c5550182e5ed903905aceeafa68196784
Manuel Martinez <manmartgarc@gmail.com>
2021-10-30 18:25:18 -0500

    add ignore comment for flake8 unused import

--- stochatreat/__init__.py
+++ stochatreat/__init__.py
@@ -7,7 +7,7 @@ Created on Wed Jul 10 12:16:55 2019
 @project:   stochatreat
 ===============================================================================
 """
-from stochatreat.stochatreat import stochatreat
+from stochatreat.stochatreat import stochatreat  # noqa: F401
 
 __version__ = '0.0.11'
 __author__ = 'Manuel Martinez'
